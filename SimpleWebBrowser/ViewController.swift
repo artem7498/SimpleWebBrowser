82f41c404b9cb590ea0b1e92f6084ada7d6e2950
Artem Akopian <artem7498@gmail.com>
2021-06-02 15:02:19 +1000

    Initial Commit

---
+++ SimpleWebBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleWebBrowser
+//
+//  Created by Артём on 6/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
